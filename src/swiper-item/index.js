1231790a4d867e9228e780bd0e93d711db0c242b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-02 16:01:51 +0000

    Deploy EdgarAllanzp/vue-h5-swiper to github.com/EdgarAllanzp/vue-h5-swiper.git:gh-pages

--- src/swiper-item/index.js
+++
@@ -1,7 +0,0 @@
-import SwiperItem from './swiper-item.vue';
-
-SwiperItem.install = function(Vue) {
-  Vue.component(SwiperItem.name, SwiperItem);
-};
-
-export default SwiperItem;
--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import { Swiper, SwiperItem } from '../src';
-
-describe('Swiper', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Swiper);
-    expect(wrapper.isVueInstance()).toBeTruthy();
-  });
-});
